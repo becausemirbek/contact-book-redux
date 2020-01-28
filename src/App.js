11c48706a3de849b07a62b855b5fb11ce2a27fa6
Mirbek Muratov <mirbekmuratov@MacBook-Air-Mirbek.local>
2020-01-28 12:49:18 +0600

    Init commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import AddTodo from './components/AddTodo'
+import List from './components/List'
+
+function App() {
+  return (
+    <div>
+      <AddTodo />
+      <List />
+    </div>
+  );
+}
+
+export default App;
